43abb70a26a0d877df2b509235af4b90c02e0b33
Matthew Sasakamoose <matt.sasakamoose@gmail.com>
2023-08-30 11:58:42 -0600

    added board files

--- scripts/generateElements.m
+++ scripts/generateElements.m
@@ -12,7 +12,7 @@ function positions = generateElements(xElements, yElements, pitch)
     index = 1;
     positions(1 : xElements * yElements) = Position(0, 0, 0);
     for x = -((xElements - 1) / 2) * pitch : pitch : ((xElements - 1) / 2) * pitch
-        for y = ((yElements - 1) / 2) * pitch : -pitch : -((yElements - 1) / 2)* pitch
+        for y = ((yElements - 1) / 2) * pitch : -pitch : -((yElements - 1) / 2) * pitch
             positions(index) = Position(x, y, 0);
             index = index + 1;
         end
